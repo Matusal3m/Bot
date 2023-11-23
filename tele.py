f2f0865cff2bca91cfbe935f6325bb7b35e597c8
Matusal3m <matusalemdedeus@gmail.com>
2023-11-23 20:43:53 -0300

    telebot

--- tele.py
+++ tele.py
@@ -1,4 +1,12 @@
 import telebot
+import threading
+import schedule
+from time import time, sleep
+from tabulate import tabulate
+from telebot.types import InlineKeyboardButton
+from telebot.types import InlineKeyboardMarkup
+from telebot.types import InlineQueryResultArticle
+from telebot.types import InputTextMessageContent
 
 chave = "6028892799:AAFiKNAdqTzqcRHXcqdoPlykDx1k255fTaU"
 chat_id = "1427119084"
@@ -8,9 +16,18 @@ bot = telebot.TeleBot(chave)
 
 @bot.message_handler(commands=["start"])
 def responder(inicio):
+    texto = """
+    Somos:
+    Kayo Lucas (https://www.instagram.com/klucasl_/)
+    Isaac Sales (https://www.instagram.com/isaac_salless/)
+    Matusalém Lima (https://www.instagram.com/m4tusalem/)
+    Gabriel Lucas
+    Estudamos na EEEP Paulo Petrola."""
+    
     bot.send_photo(inicio.from_user.id,
                    photo="https://scontent-for1-1.xx.fbcdn.net/v/t39.30808-6/402106565_907740337440208_6687529766115017951_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGHgnQIupxFBNmxHjvx3JmA7AZIGqr8SJXsBkgaqvxIlSntaP6C6YAjdV-68Va-rjfMho7fac1teUK1Sj8_rGaW&_nc_ohc=BK-qeocueMEAX9oEc0O&_nc_ht=scontent-for1-1.xx&oh=00_AfBSGj__F4h0I8P7BV9jBrJbKwrgp5yAq4smbQegkfHCTg&oe=65621DB7",
                    caption="Ola! Somos a loja Murilo Gomes, temos como objetivo vender cursos!. Digite /cursos para os nossos cursos ou /empresa para conhecer nossa empresa.")
+    bot.send_message(inicio.from_user.id, texto)
 
 
 @bot.message_handler(commands=["cursos"])
@@ -82,8 +99,8 @@ def opcao(mensagem):
 
 @bot.message_handler(commands=["murilo"])
 def opcao(menssgem):
-        bot.send_audio(menssgem.from_user.id, audio=open("murilo.m4a", 'rb',), caption="Até onde chega o meu conhecimento, Murilo Gomes não é uma divindade ou uma figura religiosa reconhecida globalmente. Parece ser um nome comum de pessoa. Se Murilo Gomes é um deus em algum contexto específico ou cultura local, eu não tenho informações sobre isso. Se você puder fornecer mais contexto ou informações sobre a divindade Murilo Gomes, eu ficarei feliz em tentar ajudar da melhor maneira possível!")
-        bot.send_photo(menssgem.from_user.id, photo="")
+        bot.send_audio(menssgem.from_user.id, audio=open("murilo.m4a", 'rb',), caption=" Murilo Gomes, o deus fictício, é uma entidade divina singular e poderosa, reverenciada por sua sabedoria transcendental e compaixão ilimitada. Sua divindade é manifestada através de uma harmonia perfeita entre o intelecto supremo e a compreensão compassiva, tornando-o um guia benevolente para seus seguidores. No panteão fictício ao qual Murilo Gomes pertence, ele é frequentemente retratado como o criador do conhecimento e da inteligência, sendo responsável por iluminar as mentes de seus adoradores. Sua presença é sentida nos momentos de busca por sabedoria e entendimento, guiando aqueles que o invocam em jornadas de autodescoberta e crescimento espiritual. A deidade Murilo Gomes é frequentemente associada a símbolos como um livro aberto, representando o conhecimento que ele compartilha com seus devotos. Seus seguidores buscam a verdade através da aprendizagem e da contemplação, acreditando que cada descoberta os aproxima mais da compreensão profunda do universo que Murilo Gomes personifica.")
+        bot.send_photo(menssgem.from_user.id, photo="https://scontent.ffor7-1.fna.fbcdn.net/v/t39.30808-6/405000392_908658004015108_5055083124652144576_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeH0V5nNu-JQNyl5pevQTzxsPqW7fkH17n0-pbt-QfXufT5VeDYFI7n2RJ_UI2mTQKPlbE1YqShL-ynaz-CjUCNR&_nc_ohc=nQGd_dtF-hIAX9vjLA1&_nc_zt=23&_nc_ht=scontent.ffor7-1.fna&oh=00_AfD3xck8sNp_7vP0yz3hJKhnXRxAWK75vO3k1R4iJP-NMw&oe=65645B51")
 
 @bot.message_handler(commands=["CompraMarketing"])
 def opcao(mensagem):
@@ -105,7 +122,6 @@ def opcao(mensagem):
 def opcao(mensagem):
     bot.reply_to(mensagem, "Compra aqui: https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjYtdmz6deCAxXtq5UCHY7uBF8QFnoECBAQAQ&url=https%3A%2F%2Fnamu.com.br%2Fcategoria%2Fyoga%2F&usg=AOvVaw3OozwKDt1r01sqyjj1-6z5&opi=89978449 ")
 
-#ENVIO DE AUDIO
 
 @bot.message_handler(commands=["Gari"])
 def opcao(menssgem):
@@ -113,11 +129,202 @@ def opcao(menssgem):
                    photo="https://scontent.ffor7-1.fna.fbcdn.net/v/t39.30808-6/404373869_122130462380067537_1224380065145938493_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHoqKEvZzksM9P_cAPnj70zDL2HsVOAJkIMvYexU4AmQo_XmmI3-74_hYsVGdym-vM-qwurXhXmbqdwc0_TsEPT&_nc_ohc=Jku65upCH7oAX8pYDXA&_nc_zt=23&_nc_ht=scontent.ffor7-1.fna&oh=00_AfBHrc69q9Q7SZQJnCqRH7pKiC1CJ590jsjiUNhCmIfC2A&oe=65613D1C",
                    caption="Este curso aborda a importância da coleta de resíduos, destacando práticas sustentáveis e métodos eficientes de gestão de resíduos. Os participantes aprenderão sobre a segurança e as melhores práticas associadas à profissão de gari, contribuindo para um ambiente mais limpo e saudável. Assine o nosso curso: /CompraGari")
 
-#@bot.message_handler(commands=["murilo"])
-    #def opcao(menssgem):
-            #bot.send_audio(menssgem.from_user.id, audio=open("murilo.m4a", 'rb'))
+
 
 bot.polling()
 
 
+config = {
 
+    "apiKey"              : "",
+    "authDomain"          : "",
+    "databaseURL"         : "",
+    "projectId"           : "",
+    "storageBucket"       : "",
+    "messagingSenderId"   : "",
+    "appId"               : "",
+    "measurementId"       : "",  
+}                                   # Your FIREBASE DATABASE configurations
+
+def emoji_board(game):              # Creates the board in ASCII format
+    emojis = {"-":"-", "x":"x","o":"o"}
+    board, temp = [], []
+    for count, _ in enumerate(game, 1):
+        if count == len(game):
+            temp.append(emojis[_])
+            board.append(temp)
+        elif count % 3 != 0:
+            temp.append(emojis[_])
+        else:
+            temp.append(emojis[_])
+            board.append(temp)
+            temp = []
+
+    return tabulate(board, tablefmt="grid")
+
+def check_win(game, game_id):       # Checks the status of the game  
+
+    wins = [
+        (0,1,2), (3,4,5), (6,7,8), (0,4,8),
+        (0,3,6), (1,4,7), (2,5,8), (2,4,6),
+    ]
+    
+    for win in wins:
+
+        C1 = (game[win[0]], game[win[1]], game[win[2]]) == ("x","x","x")
+        C2 = (game[win[0]], game[win[1]], game[win[2]]) == ("o","o","o")
+
+        if C1 or C2:
+            bot.edit_message_reply_markup(inline_message_id=game_id, reply_markup=None)
+            winner = "X" if C1 else "O"
+            bot.edit_message_text(inline_message_id=game_id, text=f"<b>Congratulations! 🎉\n\
+            \nPlayer {winner} wins! 🥳\n\n<code>{emoji_board(game)}</code></b>")
+            database.child(game_id).remove()
+            return "True"
+    else:
+        if "-" not in game: return "Draw"
+        else:   return "False"
+
+firebase = pyrebase.initialize_app(config)
+database = firebase.database()
+
+API_KEY = "6028892799:AAFiKNAdqTzqcRHXcqdoPlykDx1k255fTaU"    # API TOKEN from @BotFather
+BANNER  = ""    # FILE ID of banner for your bot
+
+bot = telebot.TeleBot(API_KEY, parse_mode="HTML")   # Initializing the bot
+
+def remove_expired():   # Deletes all expired games
+    try:
+        for game in database.get().each():
+            expiry, id = int(game.val()["expiry"]), game.val()["id"]
+            if int(time()) - expiry >= 300:
+                database.child(id).remove()
+                bot.edit_message_text(inline_message_id=id, text="<b>Game expired! 🙃</b>")
+                bot.edit_message_reply_markup(inline_message_id=id, reply_markup=None)
+    except:
+        pass
+
+def create_game_board(game):    # Creates a new empty game board
+
+    game_board, buttons = InlineKeyboardMarkup(row_width = 3), []
+
+    for pos, _ in enumerate(game, 1):
+        buttons.append(InlineKeyboardButton(_, callback_data=f'{pos}'))
+    game_board.add(*buttons)
+
+    return game_board
+
+@bot.message_handler(commands="start")
+def start(message):                     # Starts the bot
+
+    bot.send_photo(message.chat.id, BANNER, caption="<b>Wanna a play a game of Tic-Tac-Toe?\n\
+    \nClick the buton and play with your friends!</b>",
+    reply_markup = InlineKeyboardMarkup().row(InlineKeyboardButton("Play Tic-Tac-Toe!",
+    switch_inline_query="tic_tac_toe")))
+
+@bot.inline_handler(lambda query: len(query.query) == 0 or query.query == 'tic_tac_toe')   
+def send_game(query):       # Creating the inline query handler
+
+    play = InlineKeyboardMarkup().row(InlineKeyboardButton("Tap to play!",
+    callback_data=f"play{query.from_user.id}"))
+
+    try:
+        t_t_t = InlineQueryResultArticle('start_game',"丅Ꭵᑕ-丅ᗩᑕ-丅ᗝᗴ",
+        InputTextMessageContent("<b>Start the game! 🥳\n\nGame will be expire in 5 minutes!</b>",
+        parse_mode = "HTML"),reply_markup = play,
+        description = "Play a game of Tic-Tac-Toe with your friends and family! ✌🏻",
+        thumb_url = "https://github.com/TECH-SAVVY-GUY/telegram-games/blob/master/assets/tic-tac-toe.jpg?raw=true")
+        bot.answer_inline_query(query.id, [t_t_t])
+    except:
+        pass
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_listener(call):                        # A single callback listener for all calls
+
+    data, game_id = call.data, call.inline_message_id
+   
+    if data[:4] == "play":              # Starting the game
+        
+        player_x, player_o = int(data[4:]), int(call.from_user.id)
+
+        if player_o == player_x:
+            bot.answer_callback_query(call.id,
+            "⚠️ Must be a different player! ⚠️", show_alert=True)
+        else:
+            bot.edit_message_text(inline_message_id=game_id, text="<b>Game in progress!</b>")
+            bot.edit_message_reply_markup(inline_message_id=game_id,
+            reply_markup=create_game_board(["-"] * 9))
+
+            database.child(game_id).child("id").set(game_id)
+            database.child(game_id).child("player_x").set(int(data[4:]))
+            database.child(game_id).child("player_o").set(call.from_user.id)
+            database.child(game_id).child("count").set(1)
+            database.child(game_id).child("board").set(f"{['-'] * 9}")
+            database.child(game_id).child("expiry").set(int(time()))
+
+    elif data.isnumeric():      # Player move algorithm
+
+        if int(data) in range(1,10):
+
+            game = database.child(game_id).get()
+            players = [int(game.val()["player_x"]), int(game.val()["player_o"])]
+            
+            if call.from_user.id not in players:
+                bot.answer_callback_query(call.id,
+                "❌  You are not a player!  ❌", show_alert=True)
+            else: 
+                count = int(game.val()["count"])
+                
+                if count % 2 != 0:
+                    if call.from_user.id != players[0]:
+                        bot.answer_callback_query(call.id,
+                            "⚠️ Wait for your Turn! ⚠️", show_alert=True)
+                    else:
+                        board = eval(game.val()["board"])
+                        if board[int(data)-1] == "-":
+                            board[int(data)-1] = "x"
+                            bot.edit_message_reply_markup(inline_message_id=game_id,
+                            reply_markup=create_game_board(board))    
+                            stat = check_win(board, game_id)
+                            if stat != "True":
+                                if str(stat) == "Draw":
+                                    bot.edit_message_reply_markup(inline_message_id=game_id, reply_markup=None)
+                                    bot.edit_message_text(inline_message_id=game_id,
+                                    text = f"<b>It's a draw! 🥱\n\n<code>{emoji_board(board)}</code></b>")
+                                    database.child(game_id).remove()                             
+                                else:
+                                    database.child(game_id).update({"board":str(board)})
+                                    database.child(game_id).update({"count":count + 1})  
+
+                else:
+                    if call.from_user.id != players[-1]:
+                        bot.answer_callback_query(call.id,
+                            "⚠️ Wait for your Turn! ⚠️", show_alert=True)
+                    else:
+                        board = eval(game.val()["board"])
+                        if board[int(data)-1] == "-": 
+                            board[int(data)-1] = "o"
+                            bot.edit_message_reply_markup(inline_message_id=game_id,
+                            reply_markup=create_game_board(board))    
+                            stat = check_win(board, game_id)
+                            if stat != "True":
+                                if str(stat) == "Draw":
+                                    bot.edit_message_reply_markup(inline_message_id=game_id, reply_markup=None)
+                                    bot.edit_message_text(inline_message_id=game_id,
+                                    text = f"<b>It's a draw! 🥱\n\n<code>{emoji_board(board)}</code></b>")  
+                                    database.child(game_id).remove()                                                               
+                                else:
+                                    database.child(game_id).update({"board":str(board)})
+                                    database.child(game_id).update({"count":count + 1})                                   
+
+def thrd():         # Scheduling the deletion of expired games
+    while True:
+        schedule.run_pending()
+        sleep(1)
+
+schedule.every(1).minutes.do(remove_expired)
+t = threading.Thread(target=thrd)               # Creating a seperate thread
+
+def main():     # Executing all the threads
+    t.start()      
+    bot.infinity_polling()
